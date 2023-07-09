4a1d89b6ef77ea502f84ad9493f44d11597fc373
Kevy <kevy.bryan@gmail.com>
2023-07-09 12:42:15 -0500

    Implementacion de notaciones

--- BinarySearchRecursive.java
+++ BinarySearchRecursive.java
@@ -4,22 +4,25 @@ class Main {
 	}
 
 	private static int binarySearchRecursive(int num, int[] array, int low, int high) {
-	   if (low <= high) {
-		   int midP = low + (high - low) / 2;
+	   if (low <= high) {	//Checamos en dado caso que el valor que pedimos esta en el rango de valores 
+		   int midP = low + (high - low) / 2;	//Encontramos el valor medio 
 
-	   if (array[midP] == num) {
-	      return midP;
-	   } else if (array[midP] < num) {
-	      return binarySearchRecursive(num, array, midP + 1, high);
+	   if (array[midP] == num) {	//checa si el valor en el arreglo en el punto medio es igual al valor que estamos pidiendo 
+	      return midP;	// Si el valor es igual, lo regresa y acaba el codigo
+	   } else if (array[midP] < num) {	//evaluenado si el valor en el arreglo en el punto medio es menor al valor que pedimos
+	  	  // Aplicando la recursividad si el valor es mayor
+	      return binarySearchRecursive(num, array, midP + 1, high);	
 	   } else {
-	      return binarySearchRecursive(num, array, low, midP - 1);
+	   	  // Aplicando recursividad cuando el valor es menor
+	      return binarySearchRecursive(num, array, low, midP - 1);	
 	   }
 	}
 	return -1;
 	}
 
 	public static void main(String[] args) {
-	   int numbers[] = { 1, 3, 4, 89, 90, 114, 200, 222, 233, 250, 299, 300, 345, 400, 500 };
-	   System.out.println(binarySearch(4, numbers));
+		// Para usar Binary Search el arreglo tiene que estar ordenado 
+	   int numbers[] = { 1, 3, 4, 89, 90, 114, 200, 222, 233, 250, 299, 300, 345, 400, 500 };	//Arreglo ordenado
+	   System.out.println(binarySearch(5, numbers));	//Mandamos a llamar a la funcion 
 	}
 }
